c571ea1b0a793dec00b05f551ad51649895c3d34
Aki Fukuda <akif.3839@keio.jp>
2024-09-18 09:40:37 +0900

    9/18

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.apps.Todoconfig",
+    "todo.apps.TodoConfig",
 ]
 
 MIDDLEWARE = [
